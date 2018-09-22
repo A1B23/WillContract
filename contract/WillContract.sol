41598aa761d962d1f22fff8dce6a660754478687
ghub <ghub20146@gmail.com>
2018-09-22 11:47:21 +0800

    Test update procedure

--- contract/WillContract.sol
+++ contract/WillContract.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.24;
 
 contract WillContract {
     enum State {Register, Enabled, ReInit, Active, Completed, Frozen} 
+    uint registerFee = 0;    
     State state;
     uint totalFee = 0;
     address owner;
@@ -9,6 +10,12 @@ contract WillContract {
     function WillContract() {
         owner = msg.sender;
         state = State.Register;
+    }
+
+    function setFee(uint fee) {
+        require(state == State.Register);
+        require(msg.sender == owner);
+        registerFee = fee;
     }
     
 }
