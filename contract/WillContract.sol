0dd8bfd046b2c67714ff2e323772ea9427895e23
ghub <ghub20146@gmail.com>
2018-09-22 11:04:58 +0800

    Check basic change process

--- contract/WillContract.sol
+++ contract/WillContract.sol
@@ -1,5 +1,14 @@
-pragma solidity ^0.4.7;
+pragma solidity ^0.4.24;
 
 contract WillContract {
+    enum State {Register, Enabled, ReInit, Active, Completed, Frozen} 
+    State state;
+    uint totalFee = 0;
+    address owner;
+    
+    function WillContract() {
+        owner = msg.sender;
+        state = State.Register;
+    }
     
 }
